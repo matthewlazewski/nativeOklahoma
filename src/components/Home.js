aeaf3da7e2c0c7940d24b8667c6b4e8628e41a50
MattLazewski <matt.lazewski@revenueads.com>
2022-02-01 13:42:08 -0600

    added more pictures for home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,20 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './Header';
-import ImageSlider from './ImageSlider'
-import Informed from '../images/stayinformed.jpg'
+import ImageSlider from './imageSlider';
+import Informed from '../images/stayinformed.jpg';
+import NOKMag from '../images/NOKMag.jpg';
+import advertise from '../images/advertise.jpg';
+import Footer from './Footer';
 
 const Home = (props) => {
     return (
-        <Container style={{margin: '0 !important', padding: 0}}>
+        <Container className="align-items-center" style={{margin: '0 !important', padding: 0}}>
             <div class="w-100">
                 <Header/>
             </div>
-            <div>
+            <div class="home-main">
                 <ImageSlider/>
+                <div class="home-pics">
+                    <img id="stay-informed" src={Informed} alt="stay-informed"/>
+                </div>
+                <div>
+                    News and Events Go Here
+                </div>
+                <div class="home-pics">
+                    <img src={NOKMag} alt="NOK Mag" />
+                </div>
+                <div class="home-pics">
+                    <img src={advertise} alt="advertise" />
+                </div>
             </div>
-            <div class="d-flex">
-                <img id="stay-informed" src={Informed} alt="stay-informed"/>
+            <div class="w-100">
+                <Footer/>
             </div>
         </Container>
     )
