d91e023da07760fd7e002e64a3d831d87c82c149
Ashley Lazewski <ashleyadams@Ashleys-Air-3.attlocal.net>
2022-02-10 07:23:59 -0600

    added photos to nok page

---
+++ src/components/MagazineCovers/2020.js
@@ -0,0 +1,33 @@
+import January from '../../images/2020/Jan2020.jpg';
+import February from '../../images/2020/Feb2020.jpg';
+import March from '../../images/2020/March2020.jpg';
+import April from '../../images/2020/April2020.jpg';
+import May from '../../images/2020/May2020.jpg';
+import June from '../../images/2020/June2020.jpg';
+import July from '../../images/2020/July2020.jpg';
+import August from '../../images/2020/Aug2020.jpg';
+import September from '../../images/2020/Sept2020.jpg';
+import October from '../../images/2020/Oct2020.jpg';
+import November from '../../images/2020/Nov2020.jpg';
+import December from '../../images/2020/Dec2020.jpg';
+
+const Twenty2020 = () => {
+    return (
+        <div>
+            <img src={January} alt="January2020" />
+            <img src={February} alt="February2020" />
+            <img src={March} alt="March2020" />
+            <img src={April} alt="April2020" />
+            <img src={May} alt="May2020" />
+            <img src={June} alt="June2020" />
+            <img src={July} alt="July2020" />
+            <img src={August} alt="August2020" />
+            <img src={September} alt="September2020" />
+            <img src={October} alt="October2020" />
+            <img src={November} alt="November2020" />
+            <img src={December} alt="December2020" />
+        </div>
+    )
+}
+
+export default Twenty2020;
---
+++ src/components/MagazineCovers/2021.js
@@ -0,0 +1,29 @@
+import January from '../../images/2021/Jan2021.jpg';
+import February from '../../images/2021/Feb2021.jpg';
+import March from '../../images/2021/March2021.jpg';
+import April from '../../images/2021/April2021.jpg';
+import May from '../../images/2021/May2021.jpg';
+import June from '../../images/2021/June2021.jpg';
+import July from '../../images/2021/July2021.jpg';
+import August from '../../images/2021/Aug2021.jpg';
+import September from '../../images/2021/Sept2021.jpg';
+import December from '../../images/2021/Dec2021.jpg';
+
+const Twenty21 = () => {
+    return (
+        <div>
+            <img src={January} alt="January2021" />
+            <img src={February} alt="February2021" />
+            <img src={March} alt="March2021" />
+            <img src={April} alt="April2021" />
+            <img src={May} alt="May2021" />
+            <img src={June} alt="June2021" />
+            <img src={July} alt="July2021" />
+            <img src={August} alt="August2021" />
+            <img src={September} alt="September2021" />
+            <img src={December} alt="December2021" />
+        </div>
+    )
+}
+
+export default Twenty21;
