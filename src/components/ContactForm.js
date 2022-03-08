fae507d2b9a3d6092a18836218c8bb5c260a463b
MattLazewski <matt.lazewski@revenueads.com>
2022-03-08 14:43:00 -0600

    work on contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,25 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import emailjs from 'emailjs-com';
+import Swal from 'sweetalert2';
+import { Form, Input, TextArea, Button } from 'semantic-ui-react';
+
+
+const SERVICE_ID = "**************";
+const TEMPLATE_ID = "*******";
+const USER_ID = "****************";
 
 const ContactForm = () => {
 
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [message, setMessage] = useState('');
-    const [emailSent, setEmailSent] = useState(false);
-
-    const submit = () =>{
-        if (name && email && message) {
-            // TODO - send mail
-    
-            setName('');
-            setEmail('');
-            setMessage('');
-            setEmailSent(true);
-        } else {
-            alert('Please fill in all fields.');
-        }
-
-    }
+    const handleOnSubmit = (e) => {
+        e.preventDefault();
+        emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID)
+          .then((result) => {
+            console.log(result.text);
+            Swal.fire({
+              icon: 'success',
+              title: 'Message Sent Successfully'
+            })
+          }, (error) => {
+            console.log(error.text);
+            Swal.fire({
+              icon: 'error',
+              title: 'Ooops, something went wrong',
+              text: error.text,
+            })
+          });
+        e.target.reset()
+      };
 
     const isValidEmail = email => {
         const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -27,15 +36,40 @@ const ContactForm = () => {
     };
 
     return (
-        <div id="contact-form">
-            <div class="d-flex inline-flex">
-                <input class="m-2" type="text" placeholder="Name" value={name} onChange={e => setName(e.target.value)} />
-                <input class="m-2" type="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
+        <Form id="contact-form" onSubmit={handleOnSubmit}>
+            <div class="d-flex inline-flex m-2">
+                    <Form.Field
+                        id='form-input-control-email'
+                        control={Input}
+                        name='user_email'
+                        placeholder='Email'
+                        required
+                        icon='mail'
+                        iconPosition='left'
+                        class="m-3"
+                    />
+                    <Form.Field
+                        id='form-input-control-last-name'
+                        control={Input}
+                        name='user_name'
+                        placeholder='Name'
+                        required
+                        icon='user circle'
+                        iconPosition='left'
+                        class="m-3"
+                    />
             </div>
-            <textarea class="m-2" rows="5" cols="47" placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
-            <button id='submit-button'  onClick={submit}>Send Message</button>
+            <Form.Field
+                id='form-textarea-control-opinion'
+                control={TextArea}
+                cols={50}
+                name='user_message'
+                placeholder='Message…'
+                required
+            />
+            <button id='submit-button'>Send Message</button>
             {/* <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span> */}
-        </div>
+        </Form>
     );
 };
 
